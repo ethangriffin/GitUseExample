f5d200b7e9326eaf027c03bbf22880c4560c2c7d
Ethan Griffin <171957@s020rmc148-11.local>
2018-11-27 13:39:07 -0600

    New Label thingy

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,14 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Griffin, Ethan S on 11/16/18.
+//  Copyright © 2018 Griffin, Ethan S. All rights reserved.
+//
+
+import Foundation
+
+
+func sayHello() {
+    print("Hello from MapForItinerary")
+}
